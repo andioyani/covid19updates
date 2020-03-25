f6d7bdc9c15c5e24bd7c3d2d666d756a87820f28
Javier Fernandez <javierfernandez1986@gmail.com>
2020-03-25 01:54:38 -0300

    Data and Translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'covid19updates';
+  title = 'Coronavirus';
+  messagesResultsJSON:string [];
+  confirmedCases:string [];
+  countryName:string = null;
+
+  constructor (private httpService: HttpClient, private translate: TranslateService) {
+  	//console.log(navigator.language);
+	translate.setDefaultLang(navigator.language);
+  }
+
+  //messagesList:{content:string}[] = messages;
+
+  ngOnInit() {  
+  	/*
+    this.httpService.get('./assets/json/messages.json').subscribe(
+      data => {
+        this.messagesResultsJSON = data["messages"] as string [];	 // FILL THE ARRAY WITH DATA.
+        //  console.log(this.arrBirds[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+    */
+  }
+
+
+	onChange(country){
+	    this.confirmedCases = null; 	
+	   	console.log(country);
+	   	if(country != null && country != ""){
+		    this.httpService.get('https://pomber.github.io/covid19/timeseries.json').subscribe(
+		      data => {
+
+		    	data[country].sort((a, b) => {
+					return a.id < b.id ? 1 : -1; 
+	          		//return a.date < b.date ? 1 : -1;
+	     		});
+
+		    	this.confirmedCases = data[country] as string [];
+
+
+		    	console.log(this.confirmedCases);
+		        //this.messagesResultsJSON = data["messages"] as string [];	 // FILL THE ARRAY WITH DATA.
+		        //  console.log(this.arrBirds[1]);
+		      },
+		      (err: HttpErrorResponse) => {
+		        console.log (err.message);
+		      }
+		    );
+
+	   	}
+    }
+
+	useLanguage(language: string) {
+	    this.translate.use(language);
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+// import ngx-translate and the http loader
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+    	loader: {
+	        provide: TranslateLoader,
+	        useFactory: HttpLoaderFactory,
+	        deps: [HttpClient]
+    	}
+	})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
+
